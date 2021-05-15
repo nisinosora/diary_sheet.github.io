98593775be9c4b7a893ef81c26c05fb7aab4f193
KM <40517079+nisinosora@users.noreply.github.com>
2021-05-15 21:08:23 +0900

    test9 debug

--- index.js
+++ index.js
@@ -14,8 +14,8 @@ document.addEventListener('DOMContentLoaded',function(){
     /*画像サイズ設定*/
     img_width = images.naturalWidth;
     img_height = images.naturalHeight;
-    canvas.width = img_width;
-    canvas.height = img_height;
+    datas.width = img_width;
+    datas.height = img_height;
     datas.drawImage(images, 0, 0, img_width, img_height);
     var links = document.getElementById("canvased").toDataURL("image/png");
     var img = document.getElementById("diary");
