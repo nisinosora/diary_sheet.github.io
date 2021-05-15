6a0110987a2ffcefcfd3a3ee32f1473230f16152
KM <40517079+nisinosora@users.noreply.github.com>
2021-05-15 20:29:01 +0900

    test7 canvas
    canvas設定変更

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ document.addEventListener('DOMContentLoaded',function(){
   $("#make").on('click', function(){
     var datas = document.getElementById("canvased").getContext('2d');
     var images = new Image();
+    var width, height
     images.src = "./diary-sheet.jpg"
-    datas.drawImage(images, 0, 0);
+    width = images.naturalWidth;
+    height = images.naturalHeight;
+    datas.drawImage(images, 0, 0, width, height);
+    datas.width = width;
+    datas.height = height;
     var links = document.getElementById("canvased").toDataURL("image/png");
     var img = document.getElementById("diary");
     img.src = links;
