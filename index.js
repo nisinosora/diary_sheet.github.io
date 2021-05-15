ad79e0b33233d63ca93e4ac152ebae096f36efe3
KM <40517079+nisinosora@users.noreply.github.com>
2021-05-15 23:20:22 +0900

    test11
    先に画像サイズを設定するように変更

--- index.js
+++ index.js
@@ -3,19 +3,20 @@ document.addEventListener('DOMContentLoaded',function(){
     /*loading*/
     var datas = document.getElementById("canvased").getContext('2d');
     var images = new Image();
-    images.src = "./diary-sheet.jpg"
-
-    /*文字取得*/
-    var player
-    player = document.getElementById("pl");
-    datas.fillText(player.value, 870, 640);
+    images.src = "./diary-sheet.jpg";
 
     /*画像サイズ設定*/
-    var img_width, img_height
+    var img_width, img_height;
     img_width = images.naturalWidth;
     img_height = images.naturalHeight;
     datas.width = img_width;
     datas.height = img_height;
+    
+    /*文字取得*/
+    var player;
+    player = document.getElementById("pl");
+    datas.fillText(player.value, 870, 640);
+
     datas.drawImage(images, 0, 0, img_width, img_height);
     var links = document.getElementById("canvased").toDataURL("image/png");
     var img = document.getElementById("diary");
