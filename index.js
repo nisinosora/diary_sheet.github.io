9dd1f59efb1f245c03fd938aa538f9388e6ff0a0
KM <40517079+nisinosora@users.noreply.github.com>
2021-05-15 15:47:24 +0900

    test2

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 document.addEventListener('DOMContentLoaded',function(){
   $("#make").on('click', function(){
-    var datas = document.getElementById("canvas").getContext("2d");
-    var images;
-    images = new Image();
+    var datas = document.getElementById("canvased").getContext('2d');
+    var images = new Image();
     images.src = "./diary-sheet.jpg"
-    datas.drawImage(images, 0, 0);
+    datas.drawImage(images, 0, 0, 400, 500);
+    var links = document.getElementById("canvased").toDataURL("image/png");
+    alert(links);
   })
 });
