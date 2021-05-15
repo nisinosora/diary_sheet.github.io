6cf3ac960c12566d68005b31ada222c0b6831d59
KM <40517079+nisinosora@users.noreply.github.com>
2021-05-15 21:05:06 +0900

    test9

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 document.addEventListener('DOMContentLoaded',function(){
   $("#make").on('click', function(){
+    /*loading*/
     var datas = document.getElementById("canvased").getContext('2d');
     var images = new Image();
     var img_width, img_height
     images.src = "./diary-sheet.jpg"
+
+    /*文字取得*/
+    var player
+    player = document.getElementById("pl");
+    datas.fillText(player.value, 870, 640);
+
+    /*画像サイズ設定*/
     img_width = images.naturalWidth;
     img_height = images.naturalHeight;
-    var canvas = document.getElementById("canvased");
     canvas.width = img_width;
     canvas.height = img_height;
     datas.drawImage(images, 0, 0, img_width, img_height);
