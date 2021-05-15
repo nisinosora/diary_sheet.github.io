5253a8f0da9ab2e5007b9996fdc19eebd68eaa43
KM <40517079+nisinosora@users.noreply.github.com>
2021-05-15 20:57:01 +0900

    test8 debug2

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ document.addEventListener('DOMContentLoaded',function(){
   $("#make").on('click', function(){
     var datas = document.getElementById("canvased").getContext('2d');
     var images = new Image();
-    var width, height
+    var img_width, img_height
     images.src = "./diary-sheet.jpg"
-    width = images.naturalWidth;
-    height = images.naturalHeight;
+    img_width = images.naturalWidth;
+    img_height = images.naturalHeight;
     var canvas = document.getElementById("canvased");
-    canvas.setAttribute("width", width);
-    canvas.setAttribute("height", height)
-    datas.drawImage(images, 0, 0, width, height);
+    canvas.width = img_width;
+    canvas.height = img_height;
+    datas.drawImage(images, 0, 0, img_width, img_height);
     var links = document.getElementById("canvased").toDataURL("image/png");
     var img = document.getElementById("diary");
     img.src = links;
