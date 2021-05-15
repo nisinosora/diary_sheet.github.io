68821377a5a35f721c2d5d2bcd840cbf813aa3b9
KM <40517079+nisinosora@users.noreply.github.com>
2021-05-15 16:38:43 +0900

    test5

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded',function(){
     var datas = document.getElementById("canvased").getContext('2d');
     var images = new Image();
     images.src = "./diary-sheet.jpg"
-    datas.drawImage(images, 0, 0, 400, 500);
+    datas.drawImage(images, 0, 0);
     var links = document.getElementById("canvased").toDataURL("image/png");
     var img = document.getElementById("diary");
     img.src = links;
