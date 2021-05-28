be0e923a2bd2bed937a4fa17c5f5e3f309731832
KM <40517079+nisinosora@users.noreply.github.com>
2021-05-29 08:02:21 +0900

    scenario change each
    シナリオデータ記載をeachでまとめる

--- index.js
+++ index.js
@@ -24,77 +24,32 @@ document.addEventListener('DOMContentLoaded',function(){
     datas.fillText(player.value, 865, 125);//プレイヤー名
     datas.fillText(friend.value, 700, 178);//フレンズ名
     datas.fillText(medal.value, 760, 235);//シナリオメダル
-    /*シナリオ1*/
-    scenario_name = document.getElementById("scenario_name" + 1);
-    scenario_date = document.getElementById("scenario_date" + 1);
-    scenario_pg = document.getElementById("scenario_pg" + 1);
-    scenario_pl = document.getElementById("scenario_pl" + 1);
-    scenario_memo = document.getElementById("scenario_memo" + 1);
-    datas.fillText(scenario_name.value, 200, 350);
-    datas.fillText(scenario_date.value, 140, 383);
-    datas.fillText(scenario_pg.value, 120, 405);
-    line_change(datas, scenario_pl.value, 120, 427, 17, 2);
-    line_change(datas, scenario_memo.value, 90, 520, 17, 4);
 
-    /*シナリオ2*/
-    scenario_name = document.getElementById("scenario_name" + 2);
-    scenario_date = document.getElementById("scenario_date" + 2);
-    scenario_pg = document.getElementById("scenario_pg" + 2);
-    scenario_pl = document.getElementById("scenario_pl" + 2);
-    scenario_memo = document.getElementById("scenario_memo" + 2);
-    datas.fillText(scenario_name.value, 410, 350);
-    datas.fillText(scenario_date.value, 455, 383);
-    datas.fillText(scenario_pg.value, 440, 405);
-    line_change(datas, scenario_pl.value, 440, 427, 17, 2);
-    line_change(datas, scenario_memo.value, 400, 520, 17, 4);
+    //シナリオデータ取得：初期設定
+    //シナリオ名、日付、PG、PL、メモ（各X, Y）
+    //[シナリオ名_X, シナリオ名_Y, 日付_X, 日付_Y, PG_X, PG_Y, PL_X, PL_Y, Memo_X, Memo_Y]
+    let positions = [
+      [200, 350, 140, 383, 120, 405, 120, 427, 90, 520],
+      [410, 350, 455, 383, 440, 405, 440, 427, 400, 520],
+      [723, 350, 770, 383, 755, 405, 755, 427, 710, 520],
+      [95, 637, 140, 668, 120, 690, 120, 712, 80, 805],
+      [410, 635, 455, 668, 440, 690, 440, 712, 400, 805],
+      [72, 635, 770, 668, 755, 690, 755, 712, 710, 805]
+    ]
 
-    /*シナリオ3*/
-    scenario_name3 = document.getElementById("scenario_name3");
-    scenario_date3 = document.getElementById("scenario_date3");
-    scenario_pg3 = document.getElementById("scenario_pg3");
-    scenario_pl3 = document.getElementById("scenario_pl3");
-    scenario_memo3 = document.getElementById("scenario_memo3");
-    datas.fillText(scenario_name3.value, 723, 350);
-    datas.fillText(scenario_date3.value, 770, 383);
-    datas.fillText(scenario_pg3.value, 755, 405);
-    line_change(datas, scenario_pl3.value, 755, 427, 17, 2);
-    line_change(datas, scenario_memo3.value, 710, 520, 17, 4);
-
-    /*シナリオ4*/
-    scenario_name4 = document.getElementById("scenario_name4");
-    scenario_date4 = document.getElementById("scenario_date4");
-    scenario_pg4 = document.getElementById("scenario_pg4");
-    scenario_pl4 = document.getElementById("scenario_pl4");
-    scenario_memo4 = document.getElementById("scenario_memo4");
-    datas.fillText(scenario_name4.value, 95, 637);
-    datas.fillText(scenario_date4.value, 140, 668);
-    datas.fillText(scenario_pg4.value, 120, 690);
-    line_change(datas, scenario_pl4.value, 120, 712, 17, 2);
-    line_change(datas, scenario_memo4.value, 80, 805, 17, 4);
-
-    /*シナリオ5*/
-    scenario_name5 = document.getElementById("scenario_name5");
-    scenario_date5 = document.getElementById("scenario_date5");
-    scenario_pg5 = document.getElementById("scenario_pg5");
-    scenario_pl5 = document.getElementById("scenario_pl5");
-    scenario_memo5 = document.getElementById("scenario_memo5");
-    datas.fillText(scenario_name5.value, 410, 635);
-    datas.fillText(scenario_date5.value, 455, 668);
-    datas.fillText(scenario_pg5.value, 440, 690);
-    line_change(datas, scenario_pl5.value, 440, 712, 17, 5);
-    line_change(datas, scenario_memo5.value, 400, 805, 17, 4);
-
-    /*シナリオ6*/
-    scenario_name6 = document.getElementById("scenario_name6");
-    scenario_date6 = document.getElementById("scenario_date6");
-    scenario_pg6 = document.getElementById("scenario_pg6");
-    scenario_pl6 = document.getElementById("scenario_pl6");
-    scenario_memo6 = document.getElementById("scenario_memo6");
-    datas.fillText(scenario_name6.value, 723, 635);
-    datas.fillText(scenario_date6.value, 770, 668);
-    datas.fillText(scenario_pg6.value, 755, 690);
-    line_change(datas, scenario_pl6.value, 755, 712, 17, 2);
-    line_change(datas, scenario_memo6.value, 710, 805, 17, 4);
+    /*シナリオ*/
+    $.each(positions, function(index, val){
+      scenario_name = document.getElementById("scenario_name" + (index + 1));
+      scenario_date = document.getElementById("scenario_date" + (index + 1));
+      scenario_pg = document.getElementById("scenario_pg" + (index + 1));
+      scenario_pl = document.getElementById("scenario_pl" + (index + 1));
+      scenario_memo = document.getElementById("scenario_memo" + (index + 1));
+      datas.fillText(scenario_name.value, val[0], val[1]);
+      datas.fillText(scenario_date.value, val[2], val[3]);
+      datas.fillText(scenario_pg.value, val[4], val[5]);
+      line_change(datas, scenario_pl.value, val[6], val[7], 17, 2);
+      line_change(datas, scenario_memo.value, val[8], val[9], 17, 4);
+    })
 
     //フレンズリスト
     friend1 = document.getElementById("friend1");
